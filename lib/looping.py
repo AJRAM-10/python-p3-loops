74f088acbe7f70a1cba58d8d7f9952649d81021f
Anthony Ramirez <tonyjo.ramirez@gmail.com>
2023-06-16 15:35:18 -0400

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    stupid_list = list()
+    for i in int_list:
+        stupid_list.append(i**2)
+    return stupid_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    
+    for i in range(101):
+        if i > 0 and (i % 3 == 0 and i % 5 == 0):
+            print("FizzBuzz")
+        elif i > 0 and i % 3 == 0:
+            print("Fizz")
+        elif i > 0 and i % 5 == 0:
+            print("Buzz")
+        elif i > 0:
+            print(f"{i}")
